3c70185a494cb540c5fae4bd7566eed7a5f14533
Geoff Smith <smith260194@gmail.com>
2019-05-15 22:25:08 +0100

    Garage can store bikes

--- lib/garage.rb
+++ lib/garage.rb
@@ -1,4 +1,9 @@
 class Garage
+  attr_reader :bikes
+  def initialize
+    @bikes = []
+  end
   def receive_faulty_bikes(faulty_bikes)
+    bikes.concat(faulty_bikes)
   end
 end
--- spec/garage_spec.rb
+++ spec/garage_spec.rb
@@ -6,4 +6,13 @@ describe Garage do
 
     expect(my_garage).to respond_to(:receive_faulty_bikes).with(1).argument
   end
+
+  it 'can store the faulty bikes it receives' do
+    my_garage = Garage.new
+    first_faulty_bike = double(:bike, working?: false)
+    second_faulty_bike = double(:bike, working?: false)
+    my_garage.receive_faulty_bikes([first_faulty_bike, second_faulty_bike])
+
+    expect(my_garage.bikes).to eq([first_faulty_bike, second_faulty_bike])
+  end
 end
