b296ebafc1cf441675b194cee15980dd887d1f1c
Geoff Smith <smith260194@gmail.com>
2019-05-15 14:23:33 +0100

    Storing bikes in an arry

--- lib/van.rb
+++ lib/van.rb
@@ -1,6 +1,10 @@
 class Van
   attr_reader :bikes
+  def initialize
+    @bikes = []
+  end
+#guard case / type check  
   def collect_bikes_from(bike_receptacle)
-    @bikes = bike_receptacle.give_faulty_bikes
+    @bikes.concat(bike_receptacle.give_faulty_bikes)
   end
 end
--- spec/van_spec.rb
+++ spec/van_spec.rb
@@ -9,10 +9,19 @@ describe Van do
 
   it 'will store the result of telling bike receptacle to give bikes' do
     my_van = Van.new
-    bike_receptacle = double(:bike_receptacle, give_faulty_bikes: "faulty bike")
+    bike_receptacle = double(:bike_receptacle, give_faulty_bikes: ["faulty bike"])
 
     my_van.collect_bikes_from(bike_receptacle)
 
-    expect(my_van.bikes).to eq("faulty bike")
+    expect(my_van.bikes).to eq(["faulty bike"])
+  end
+
+  it 'can store multiple bikes receieved from bike receptacle' do
+    my_van = Van.new
+    bike_receptacle = double(:bike_receptacle, give_faulty_bikes: ["faulty bike 1", "faulty bike 2"])
+
+    my_van.collect_bikes_from(bike_receptacle)
+
+    expect(my_van.bikes).to eq(["faulty bike 1", "faulty bike 2"])
   end
 end
