682fad9a2c49cbdcf7f52485b15394ba310e9c9e
Geoff Smith <smith260194@gmail.com>
2019-05-15 05:54:07 +0100

    Allowing multiple bikes and limiting total capacity

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,32 @@
 require 'bike'
 
 class DockingStation
+  DEFAULT_CAPACITY = 20
+  attr_reader :docked_bikes
+
+  def initialize
+    @docked_bikes = []
+  end
+
   def release_bike
-    Bike.new
+    raise 'No bikes available' if empty?
+
+    @docked_bikes.pop
+  end
+
+  def dock(bike)
+    raise 'Docking station at capacity' if full?
+
+    @docked_bikes.push(bike)
+  end
+
+  private
+
+  def full?
+    @docked_bikes.count >= DEFAULT_CAPACITY
+  end
+
+  def empty?
+    @docked_bikes.count <= 0
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,6 +1,8 @@
 require 'docking_station'
+require 'bike'
 
 describe DockingStation do
+  default_capacity = DockingStation::DEFAULT_CAPACITY
   it { is_expected.to respond_to(:release_bike) }
 
   it 'will respond to release_bike' do
@@ -11,6 +13,8 @@ describe DockingStation do
 
   it 'gets a bike' do
     my_docking_station = DockingStation.new
+    my_bike = Bike.new
+    my_docking_station.dock(my_bike)
 
     my_bike = my_docking_station.release_bike
 
@@ -19,9 +23,60 @@ describe DockingStation do
 
   it 'gets a bike that is working' do
     my_docking_station = DockingStation.new
-
+    my_bike = Bike.new
+    my_docking_station.dock(my_bike)
     my_bike = my_docking_station.release_bike
 
     expect(my_bike.working?).to eq(true)
+  end
+
+  it 'will respond to dock with an argument' do
+    my_docking_station = DockingStation.new
+
+    expect(my_docking_station).to respond_to(:dock).with(1).argument
+  end
+
+  it 'will respond to bike' do
+    my_docking_station = DockingStation.new
+
+    expect(my_docking_station).to respond_to(:docked_bikes)
+  end
+
+  it 'docks the bike we dock' do
+    my_docking_station = DockingStation.new
+    my_bike = Bike.new
+
+    expect(my_docking_station.dock(my_bike)).to eq([my_bike])
+  end
+
+  it 'returns a docked bike' do
+    my_docking_station = DockingStation.new
+    bike = Bike.new
+
+    my_docking_station.dock(bike)
+
+    expect(my_docking_station.release_bike).to eq(bike)
+  end
+
+  it 'raises error if no bike docked' do
+    my_docking_station = DockingStation.new
+
+    expect { my_docking_station.release_bike }.to
+    raise_error 'No bikes available'
+  end
+
+  it 'raises error if docking station is at capacity' do
+    my_docking_station = DockingStation.new
+    my_docking_station.dock(Bike.new)
+    expect { my_docking_station.dock Bike.new }.to_not raise_error
+  end
+
+  it 'raises error if docking station has more than 20 bikes in' do
+    my_docking_station = DockingStation.new
+    default_capacity.times do
+      my_docking_station.dock(Bike.new)
+    end
+    expect { my_docking_station.dock Bike.new }.to
+    raise_error 'Docking station at capacity'
   end
 end
