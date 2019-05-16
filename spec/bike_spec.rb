d907562164c897f07b7ed8c2670c1d859895fb41
Geoff Smith <smith260194@gmail.com>
2019-05-16 13:22:04 +0100

    Setting up each test with a bike

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,31 +1,28 @@
 require 'bike'
 
 describe Bike do
-  it 'responds to working?' do
-    my_bike = Bike.new
+  let!(:bike) do
+    @my_bike = Bike.new
+  end
 
-    expect(my_bike).to respond_to(:working?)
+  it 'responds to working?' do
+    expect(@my_bike).to respond_to(:working?)
   end
 
   it 'responds to broken' do
-    my_bike = Bike.new
-
-    expect(my_bike).to respond_to(:is_broken)
+    expect(@my_bike).to respond_to(:is_broken)
   end
 
   it 'says bike is not working if bike is broken' do
-    my_bike = Bike.new
+    @my_bike.is_broken
 
-    my_bike.is_broken
-
-    expect(my_bike.working?).to be false
+    expect(@my_bike.working?).to be false
   end
 
   it 'can be fixed' do
-    my_bike = Bike.new
-    my_bike.is_broken
-    my_bike.fix
+    @my_bike.is_broken
+    @my_bike.fix
 
-    expect(my_bike.working?).to be true
+    expect(@my_bike.working?).to be true
   end
 end
